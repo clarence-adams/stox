7ead9b8bd1f68aeb1c7e1cd91890e5543be9dff2
clarence-de-coder <clarence.e.a@gmail.com>
2021-07-09 23:14:29 -0400

    Quote component will display most current average stock price if a valid symbol is submitted

---
+++ client/src/Quote.js
@@ -0,0 +1,38 @@
+import './Quote.css';
+import {useState} from 'react'
+
+function Quote() {
+  const [symbol, setCurrentSymbol] = useState('')
+  const [currentQuote, setCurrentQuote] = useState('')
+
+  const changeHandler = (event) => setCurrentSymbol(event.target.value.toUpperCase())
+
+  const data = {symbol}
+  const requestOptions = {
+    method: "POST",
+    headers: {"Content-Type": "application/json"},
+    body: JSON.stringify(data)
+  }
+
+  const clickHandler = () => {
+    fetch('/dashboard/quote', requestOptions)
+    .then(res => res.json())
+    .then(res => {
+      setCurrentQuote(res.quote)
+    })
+    .catch(() => {
+      setCurrentQuote('error')
+    })
+  }
+
+  return (
+    <div id='quote-wrapper'>
+      <label htmlFor='quote'/>
+      <input type='text' id='symbol' onChange={changeHandler} value={symbol}/>
+      <button type='submit' onClick={clickHandler}>Get Quote</button>
+      <p>{currentQuote}</p>
+    </div>
+  )
+}
+
+export default Quote;
--- server/index.js
+++ server/index.js
@@ -1,19 +1,52 @@
-const path = require('path');
-const express = require('express');
+const path = require('path')
+const express = require('express')
+const bodyParser = require('body-parser')
+const got = require('got')
 
-const PORT = process.env.PORT || 3001;
-const API_KEY = process.env.API_KEY;
+require('dotenv').config();
 
-const app = express();
+const PORT = process.env.PORT || 3001
+const API_KEY = process.env.API_KEY
 
-// Have Node serve the files for our built React app
-app.use(express.static(path.resolve(__dirname, '../client/build')));
+const app = express()
 
-// All other GET requests not handled before will return our React app
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
-});
+app.use(bodyParser.json())
+
+// landing page
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/public/landing.html'))
+})
+
+// login page
+
+app.get('/authentication', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/public/authentication.html'))
+})
+
+// dashboard
+
+app.use(express.static(path.resolve(__dirname, '../client/build')))
+
+app.get('/dashboard', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'))
+})
+
+app.use('/dashboard/quote', bodyParser.urlencoded({extended: false}))
+
+app.post('/dashboard/quote', (req, res) => {
+  (async () => {
+    try {
+      const response = await got(
+        'https://cloud.iexapis.com/stable/stock/' + req.body.symbol + '/intraday-prices?token=' + API_KEY + '&chartLast=1'
+      ).json()
+      res.json({quote: response[0].average})
+    } catch (error) {
+      console.log(error.response.body)
+    }
+  })()
+})
 
 app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
-});
+  console.log(`Server listening on ${PORT}`)
+})
