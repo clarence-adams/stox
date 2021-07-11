25e2ecbb85857c6a3f1d5d47c409ee4f78503d30
Clarence <clarence.e.a@gmail.com>
2021-07-11 20:18:24 +0000

    Added authentication still working on logging out

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,46 @@
 import './App.css';
+import {useState, useEffect} from 'react'
 import Quote from './Quote.js'
 
 function App() {
+  const [user, setUser] = useState({})
+
+  useEffect(() => {
+    const requestOptions = {
+      method: "GET",
+      headers: {"Content-Type": "application/json"}
+    }
+
+    fetch('/dashboard/user', requestOptions)
+    .then(res => res.json())
+    .then(res => {
+      setUser(res)
+    })
+    .catch(() => {
+      setUser('error')
+    })
+  }, [])
+
+  const logout = () => {
+     const requestOptions = {
+      method: "GET",
+      headers: {"Content-Type": "application/json"},
+      redirect: 'follow'
+    }
+
+    fetch('/dashboard/logout', requestOptions)
+    .catch(() => {
+      console.log('logout error')
+    })
+  }
+
   return (
     <div className="App">
+      <div id='navbar'>
+        <button id='logout-button' onClick={logout}>Logout</button>
+      </div>
+      <h1>Hello, {user.username}</h1>
+      <h2>Cash on hand: {user.cash}</h2>
       <Quote/>
     </div>
   );
--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 const path = require('path')
 const express = require('express')
 const bodyParser = require('body-parser')
+const cookieSession = require('cookie-session')
 const got = require('got')
 const mongoose = require('mongoose')
 const {Schema} = mongoose
+const hashFunction = require('./hashFunction.js')
 
 // configure the use of .env file and and initializes global variables
 
@@ -12,6 +14,9 @@ require('dotenv').config();
 const PORT = process.env.PORT || 3001
 const API_KEY = process.env.API_KEY
 const MONGO_URI = process.env.MONGO_URI
+
+const PUBLIC_FOLDER = path.resolve(__dirname, '../client/public')
+const REACT_BUILD = path.resolve(__dirname, '../client/build')
 
 // connects to MongoDB and logs an error if there is an error
 
@@ -24,39 +29,137 @@ mongoose.connect(
 .catch(err => console.log(err))
 
 const userSchema = new Schema ({
-  name: String,
-  age: Number,
-  favoriteFoods: [String]
+  id: Number,
+  username: String,
+  password: String,
+  cash: Number,
+  positions: [{symbol: String, shares: Number, shareValue: Number, date: Date}],
 })
-
 const User = mongoose.model('User', userSchema);
 
 const app = express()
 
-app.use(bodyParser.json())
+// starts middleware for body parsing, static file hosting, and sessions
 
+app.use(bodyParser.json())
+app.use('/static', express.static(PUBLIC_FOLDER))
+app.use(cookieSession({
+  name: 'session',
+  secret: 'super duper secret'
+}))
+
+//
 // landing page
+//
 
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../client/public/landing.html'))
+  res.sendFile(path.resolve(PUBLIC_FOLDER, 'landing.html'))
 })
 
-// login page
+// registration endpoint
+app.use('/register', bodyParser.urlencoded({extended: false}))
+app.post('/register', (req, res) => {
+  
+  const newUser = new User(
+    {
+      username: req.body.username,
+      password: hashFunction.SHA256(req.body.password),
+      cash: 10000,
+      positions: []
+    }
+  )
+
+  newUser.save((err, data) => {
+    if (err) {
+      return console.log(err)
+    } else {
+      res.redirect(301, '/authentication')
+    }
+  })
+})
+
+//
+// authentication page
+//
 
 app.get('/authentication', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../client/public/authentication.html'))
+  if (req.session.authenticated === true) {
+    console.log('redirected')
+    res.redirect(301, '/dashboard')
+  } else {
+    res.sendFile(path.resolve(PUBLIC_FOLDER, 'authentication.html'))
+  }
 })
 
-// dashboard
+// authentication endpoint
+app.use('/authenticate', bodyParser.urlencoded({extended: false}))
+app.post('/authenticate', (req, res) => {
+  User.find({username: req.body.username}, (err, user) => {
+    const username = user[0].username
+    const userPassword = user[0].password
+    const enteredPassword = hashFunction.SHA256(req.body.password)
 
-app.use(express.static(path.resolve(__dirname, '../client/build')))
+    if (err) {
+      return console.log(err)
+    } else {
+      if (userPassword === enteredPassword) {
+        req.session.authenticated = true
+        req.session.username = username
+        res.redirect(301, '/dashboard')
+      } else {
+        res.json({error: 'incorrect password'})
+      }
+    }
+  })
+})
+
+//
+// dashboard
+//
+
+app.use(express.static(REACT_BUILD))
 
 app.get('/dashboard', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'))
+  if (req.session.authenticated === true) {
+    res.sendFile(path.resolve(REACT_BUILD, 'index.html'))
+  } else {
+    res.redirect(301, '/authentication')
+  }
 })
 
-app.use('/dashboard/quote', bodyParser.urlencoded({extended: false}))
+// user info
+app.get('/dashboard/user', (req, res) => {
+  if (req.session.authenticated === true) {
+    User.find({username: req.session.username}, (err, user) => {
+      const authenticated = req.session.authenticated
+      const username = user[0].username
+      const cash = user[0].cash
+      const positions = user[0].positions
+      if (err) {
+        return console.log(err)
+      } else {
+        res.json({
+          authenticated: authenticated,
+          username: username,
+          cash: cash,
+          positions: positions
+        })
+      }
+    })
+  } else {
+    res.json({error: 'you must log in first!'})
+  }
+})
 
+// logout endpoint
+app.get('/dashboard/logout', (req, res) => {
+  req.session = null
+  console.log('session destroyed')
+  res.redirect(301, '/authentication')
+})
+
+// stock quote
+app.use('/dashboard/quote', bodyParser.urlencoded({extended: false}))
 app.post('/dashboard/quote', (req, res) => {
   (async () => {
     try {
