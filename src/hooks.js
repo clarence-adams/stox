97f5544eaa105022a3defec3cfcd0701c9a9eaae
clarence-adams <clarence.e.a@gmail.com>
2022-06-30 07:35:22 -0400

    Commented out hooks.js

--- src/hooks.js
+++ src/hooks.js
@@ -1,33 +1,33 @@
-import * as cookie from 'cookie';
-import jwt from 'jsonwebtoken';
+// import * as cookie from 'cookie';
+// import jwt from 'jsonwebtoken';
 
-export async function handle({ event, resolve }) {
-	// redirect users with no valid token to the landing page
-	if (
-		event.url.pathname.startsWith('/dashboard') ||
-		event.url.pathname.startsWith('/api/dashboard')
-	) {
-		const cookies = cookie.parse(event.request.headers.get('cookie') || '');
-		const authToken = cookies.authToken;
+// export async function handle({ event, resolve }) {
+// 	// redirect users with no valid token to the landing page
+// 	if (
+// 		event.url.pathname.startsWith('/dashboard') ||
+// 		event.url.pathname.startsWith('/api/dashboard')
+// 	) {
+// 		const cookies = cookie.parse(event.request.headers.get('cookie') || '');
+// 		const authToken = cookies.authToken;
 
-		if (authToken === undefined) {
-			return new Response({
-				status: 303,
-				headers: { Location: '/' }
-			});
-		}
+// 		if (authToken === undefined) {
+// 			return new Response({
+// 				status: 303,
+// 				headers: { Location: '/' }
+// 			});
+// 		}
 
-		try {
-			jwt.verify(authToken, import.meta.env.VITE_ACCESS_TOKEN_SECRET);
-		} catch (err) {
-			return new Response('Redirect', {
-				status: 303,
-				headers: { Location: '/' }
-			});
-		}
-	}
+// 		try {
+// 			jwt.verify(authToken, import.meta.env.VITE_ACCESS_TOKEN_SECRET);
+// 		} catch (err) {
+// 			return new Response('Redirect', {
+// 				status: 303,
+// 				headers: { Location: '/' }
+// 			});
+// 		}
+// 	}
 
-	const response = await resolve(event);
-	console.log(response);
-	return response;
-}
+// 	const response = await resolve(event);
+// 	console.log(response);
+// 	return response;
+// }
