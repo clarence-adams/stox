33b6617d8fd71c0c539dea4d13a979c7c43385f9
clarence-adams <clarence.e.a@gmail.com>
2022-07-01 07:46:04 -0400

    Reintroduced route protection in hooks.js

--- src/hooks.js
+++ src/hooks.js
@@ -3,23 +3,23 @@
 
 export async function handle({ event, resolve }) {
 	// redirect users with no valid token to the landing page
-	// if (
-	// 	event.url.pathname.startsWith('/dashboard') ||
-	// 	event.url.pathname.startsWith('/api/dashboard')
-	// ) {
-	// 	const cookies = cookie.parse(event.request.headers.get('cookie') || '');
-	// 	const authToken = cookies.authToken;
+	if (
+		event.url.pathname.startsWith('/dashboard') ||
+		event.url.pathname.startsWith('/api/dashboard')
+	) {
+		const cookies = cookie.parse(event.request.headers.get('cookie') || '');
+		const authToken = cookies.authToken;
 
-	// 	if (authToken === undefined) {
-	// 		return Response.redirect(baseUrl + '/login', 303);
-	// 	}
+		if (authToken === undefined) {
+			return Response.redirect(baseUrl + '/login', 303);
+		}
 
-	// 	try {
-	// 		jwt.verify(authToken, import.meta.env.VITE_ACCESS_TOKEN_SECRET);
-	// 	} catch (err) {
-	// 		return Response.redirect(baseUrl + '/login', 303);
-	// 	}
-	// }
+		try {
+			jwt.verify(authToken, import.meta.env.VITE_ACCESS_TOKEN_SECRET);
+		} catch (err) {
+			return Response.redirect(baseUrl + '/login', 303);
+		}
+	}
 
 	const response = await resolve(event);
 	// console.log(response);
